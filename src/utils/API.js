fddf22b44a7e3e541d1efe9c47bdb6315881a8db
Jenn Greiner <jenn.greiner1@gmail.com>
2021-05-26 11:31:53 -0600

    Updates

--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const URL = "https://randomuser.me/api/?results=15&nat=us";
-// eslint-disable-next-line
-export default {
-  search: function () {
-    return axios.get(URL);
-  },
-};
